8eb2e69f0cd174df09a835acac6c51be6a3ba618
Abdo Alexandre <alexandre.abdo@etud.univ-jfc.fr>
2019-11-06 22:48:28 -0500

    Managed to pull data from device through bluetooth

--- src/app/push-bluetooth/push-bluetooth.page.ts
+++ src/app/push-bluetooth/push-bluetooth.page.ts
@@ -10,7 +10,7 @@ import { SubjectSubscriber } from 'rxjs/internal/Subject';
   styleUrls: ['./push-bluetooth.page.scss'],
 })
 export class PushBluetoothPage implements OnInit {
-  pairedList: any;
+  pairedList: pairedList;
   listToggle: boolean = false;
   pairedDeviceID : number = 0;
 
@@ -38,8 +38,49 @@ export class PushBluetoothPage implements OnInit {
     })
   }
 
+  selectDevice(){
+    let connectedDevice = this.pairedList[this.pairedDeviceID];
+    if(!connectedDevice.address){
+      this.showToast("Select paired device to connect");
+      return;
+    }
+    let address = connectedDevice.address;
+    let name = connectedDevice.name
+
+    this.connect(address);
+  }
+
+  connect(address){
+    this.bluetoothSerial.connect(address).subscribe(success =>{
+      this.deviceConnected();
+      this.showToast("Successfully connected");
+      this.deviceConnected();
+    }, error=>{
+      this.showToast(error);
+    });
+  }
+
+  deviceConnected(){
+    this.bluetoothSerial.subscribe('ah\n').subscribe(success =>{
+      this.showToast(success);
+    }, error =>{
+      this.showToast(error);
+    });
+  }
+
   getLastFile() {
-    return new Promise(resolve => {
+    this.bluetoothSerial.write("get_previous\n").then(success =>{
+      this.bluetoothSerial.read().then(success =>{
+        this.showToast(success);
+      }, error =>{
+        this.showToast(error);
+      });
+    }, error =>{
+      this.showToast(error);
+    });
+
+    /*return new Promise(resolve => {
+      
       //TODO: Connect to bluetooth and pull data
       let body = {
         r: 10,
@@ -51,7 +92,7 @@ export class PushBluetoothPage implements OnInit {
       this.postProvider.postData(body, "RecordLight.php").subscribe(data => {
         this.showToast(data);
       });
-    });
+    });*/
   }
 
   async showToast(data){
@@ -62,4 +103,11 @@ export class PushBluetoothPage implements OnInit {
     });
     toast.present();
   }
+
+}
+interface pairedList{
+  "class": number,
+  "id": string,
+  "address": string,
+  "name": string
 }
